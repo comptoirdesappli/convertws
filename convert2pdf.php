22451bd1c3b82fd4ac60eb4dc7ddb1d20f0ff23b
Guillaume <sharevb@gmail.com>
2019-03-20 08:52:37 +0100

    Clean convert2pdf

--- convert2pdf.php
+++ convert2pdf.php
@@ -8,16 +8,16 @@
 function process_file( $filename ) {
 	require_once 'runners/LibreOfficeConverter.php';
 
-	$pdffile   = $filename . '.pdf';
-	$pdfname   = pathinfo( $_FILES['input']['name'], PATHINFO_FILENAME ) . '.pdf';
-	$converter = new LibreOfficeConverter( $filename );
-	$converter->convert( $pdffile );
+	$temp_pdf_filename = BaseRunner::replace_extension( $filename, '.pdf' );
+	$output_pdf_name   = pathinfo( $_FILES['input']['name'], PATHINFO_FILENAME ) . '.pdf';
+	$converter         = new LibreOfficeConverter( $filename );
+	$converter->convert( $temp_pdf_filename );
 
 	header( "Content-type:application/pdf" );
-	header( "Content-Disposition:attachment;filename='$pdfname'" );
+	header( "Content-Disposition:attachment;filename=\"$output_pdf_name\"" );
 
-	readfile( $pdffile );
-	@unlink( $filename );
+	readfile( $temp_pdf_filename );
+	@unlink( $temp_pdf_filename );
 }
 
 //run process_file function
